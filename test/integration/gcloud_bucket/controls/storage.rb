c8c7bae2f56f8807f255cbad7a845b776396f292
Aaron Lane <aaron.lane@newcontext.com>
2018-05-26 11:02:02 -0400

    Expand descriptions of control

--- test/integration/gcloud_bucket/controls/storage.rb
+++ test/integration/gcloud_bucket/controls/storage.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
-control "bucket" do
-  describe command('gsutil ls') do
-    its('stdout') { should match (/gs:\/\/test-org-state-bucket\//) }
-  end
+bucket_name = attribute "bucket_name", {}
 
-  describe command('gsutil ls -L -b gs://test-org-state-bucket') do
-    its('stdout') { should match (/Storage class:.*STANDARD/) }
-    its('stdout') { should match (/Versioning enabled:.*True/) }
+control "bucket" do
+  describe "bucket" do
+    subject do
+      command "gsutil ls -L -b gs://#{bucket_name}"
+    end
+
+    specify "should be an instance of the standard storage class" do
+      expect(subject.stdout).to match /Storage class:.*STANDARD/
+    end
+
+    specify "should be versioned" do
+      expect(subject.stdout).to match /Versioning enabled:.*True/
+    end
   end
 end
